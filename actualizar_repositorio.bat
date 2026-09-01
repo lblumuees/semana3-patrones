634442e39d166d2fac33bb2f376d09042985f9dd
Luis <luis.blum@uees.edu.ec>
2026-08-31 22:21:56 -0500

    actualizar README

--- actualizar_repositorio.bat
+++ actualizar_repositorio.bat
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "incluir clases builder"
+git commit -m "actualizar README"
 git branch -M main
 git remote add origin https://github.com/lblumuees/semana3-patrones.git
 git push -u origin main
